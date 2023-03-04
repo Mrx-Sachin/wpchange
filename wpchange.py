68933c9475e7049d437167cddffcc8d852acd0ab
sachin <112502093+Mrx-Sachin@users.noreply.github.com>
2023-03-04 16:23:02 +0530

    auto input
    it will now work in the background if the user don't give any input for 3 seconds

--- wpchange.py
+++ wpchange.py
@@ -1,20 +1,27 @@
 import datetime
 import ctypes
 import time
+import msvcrt
 import os
 from tkinter import filedialog
 from tkinter import *
 
 
 location=''
-def changewallpaper(file):
+def changewallpaper(filepath):
     today = datetime.datetime.now()
-    date_time = today.strftime("%H")
-    if(int(date_time)<10):
-    	date_time=int(date_time)%10
-    path = file +"\\"+str(date_time)+'.jpg'
-    ctypes.windll.user32.SystemParametersInfoW(20, 0, path , 0)
-    print("wallpaper set sucesfully")
+    date_time = int(today.strftime("%H"))
+    if date_time < 10:
+        date_time %= 10
+    imgpath = os.path.join(filepath, f"{date_time}.jpg")
+    if os.path.exists(imgpath):
+        try:
+            ctypes.windll.user32.SystemParametersInfoW(20, 0, imgpath, 0)
+            print("Wallpaper set successfully")
+        except:
+            print("Error setting wallpaper")
+    else:
+        print(f"Image not found: {imgpath}")
 
 def select_folder():
     global location
@@ -42,10 +49,33 @@ def print_path():
     file1.close()
     
 
-# s=input(" Do you want to select foler (y/n) \n ")
-s='n'
-if(s=="y"):
-    select_folder()
-    prev_path()
-else:
-    print_path()
+
+
+
+timeout = 3
+start_time = time.time()
+
+while True:
+    if msvcrt.kbhit():
+        key = msvcrt.getch()
+
+        if key == b'y':
+            # print("y")
+            select_folder()
+            prev_path()
+            break
+        else:
+            # print("n")
+            print_path()
+            break
+
+    elapsed_time = time.time() - start_time
+    if elapsed_time >= timeout:
+        print("Timed out, using default value of 'n'")
+        print_path()
+        break
+
+
+
+
+# print("Stopped typing")
